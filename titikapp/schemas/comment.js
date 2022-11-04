e34e0ef52d976b159d4da0d979bad65a1c5212cb
George <gex7777@gmail.com>
2022-11-04 22:35:50 +0530

    initial setup

---
+++ titikapp/schemas/comment.js
@@ -0,0 +1,17 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "postedBy",
+      titlw: "Posted By",
+      type: "string",
+    },
+    {
+      name: "comment",
+      title: "Comment",
+      type: "string",
+    },
+  ],
+};
---
+++ titikapp/schemas/user.js
@@ -0,0 +1,17 @@
+export default {
+  name: "user",
+  title: "user",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      titlw: "User Name",
+      type: "string",
+    },
+    {
+      name: "image",
+      title: "Image",
+      type: "string",
+    },
+  ],
+};
