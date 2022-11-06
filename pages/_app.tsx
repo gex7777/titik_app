82c0d459f4a53f6e8b4ee595155e11b9e3bce956
George <gex7777@gmail.com>
2022-11-06 19:01:50 +0530

    initial setup 3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { useState, useEffect } from "react";
+import Navbar from "./../components/Navbar";
+import Sidebar from "../components/Sidebar";
 
 export const App = ({ Component, pageProps }: AppProps) => {
   const [isSSR, setIsSSR] = useState(true);
@@ -9,5 +11,17 @@ export const App = ({ Component, pageProps }: AppProps) => {
   }, []);
   if (isSSR) return null;
 
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Navbar />
+      <div className="flex gap-6 md:gap-20">
+        <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+          <Sidebar />
+        </div>
+        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </>
+  );
 };
